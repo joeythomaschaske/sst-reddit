032f68adbf0736607581437b6b9fa40cf351dbeb
Joseph Thomaschaske <joseph.thomaschaske@komodohealth.com>
2023-02-24 13:27:12 -0600

    update never

--- services/core/article.ts
+++ services/core/article.ts
@@ -4,7 +4,7 @@ import { ulid } from "ulid";
 import { SQL } from "./sql";
 
 export async function create(title: string, url: string) {
-  const [result] = await SQL.DB.insertInto("article")
+  const [result] = await SQL.DB.insertInto("article" as never)
     .values({ articleID: ulid(), url, title })
     .returningAll()
     .execute();
@@ -12,21 +12,21 @@ export async function create(title: string, url: string) {
 }
 
 export function get(articleID: string) {
-  return SQL.DB.selectFrom("article")
+  return SQL.DB.selectFrom("article" as never)
     .selectAll()
-    .where("articleID", "=", articleID)
+    .where("articleID", "=", articleID as never)
     .executeTakeFirst();
 }
 
 export function list() {
-  return SQL.DB.selectFrom("article")
+  return SQL.DB.selectFrom("article" as never)
     .selectAll()
     .orderBy("created", "desc")
     .execute();
 }
 
 export function addComment(articleID: string, text: string) {
-  return SQL.DB.insertInto("comment")
+  return SQL.DB.insertInto("comment" as never)
     .values({
       commentID: ulid(),
       articleID,
@@ -37,8 +37,8 @@ export function addComment(articleID: string, text: string) {
 }
 
 export function comments(articleID: string) {
-  return SQL.DB.selectFrom("comment")
+  return SQL.DB.selectFrom("comment" as never)
     .selectAll()
-    .where("articleID", "=", articleID)
+    .where("articleID", "=", articleID as never)
     .execute();
 }
